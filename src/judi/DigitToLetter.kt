64203d03aaf64eab5196afc31fd05d028a4137dd
jbayoli <31174863+jbayoli@users.noreply.github.com>
2018-05-18 12:55:56 +0100

    Modify some functions and add processHundred and processThousands functions

--- src/judi/DigitToLetter.kt
+++ src/judi/DigitToLetter.kt
@@ -1,222 +1,158 @@
 package judi
 
 class DigitToLetter {
-    private var _digit: String = ""
-    private var _digit2: String = ""
-    private var _digit3: String = ""
-    private var _lastDigitInString: String = ""
-    private var _digitInSting: String = ""
-    private var _numberInLetter = ""
-    private val _d = mapOf(0 to "Zéro",1 to "Un", 2 to "Deux", 3 to "Trois", 4 to "Quatre", 5 to "Cinq", 6 to "Six", 7 to "Sept", 8 to "Huit", 9 to "Neuf",
-            10 to "Dix", 11 to "Onze", 12 to "Douze", 13 to "Treize", 14 to "Quatorze", 15 to "Quinze", 16 to "Seize", 20 to "Vingt", 30 to "Trente",
-            40 to "Quarante", 50 to "Cinquante", 60 to "Soixante", 100 to "Cent", 1_000 to "Mille")
+    private val _mapOfKnewDigit = mapOf(0 to "Zéro",1 to "Un", 2 to "Deux", 3 to "Trois", 4 to "Quatre", 5 to "Cinq", 6 to "Six", 7 to "Sept", 8 to "Huit",
+            9 to "Neuf", 10 to "Dix", 11 to "Onze", 12 to "Douze", 13 to "Treize", 14 to "Quatorze", 15 to "Quinze", 16 to "Seize", 20 to "Vingt", 30 to "Trente",
+            40 to "Quarante", 50 to "Cinquante", 60 to "Soixante", 100 to "Cent", 1_000 to "Mille", 1_000_000 to "Un million")
 
-    fun digitToLetter(args: Int): String {
-        mainProcess(args)
-        return _numberInLetter
+    fun digitToLetter(args: Int = 0): String {
+        return if (args >= 0) {
+            mainProcess(args)
+        } else {
+            "Les nombres négatifs ne sont pas pris en charge "
+        }
     }
 
-    private fun checkValueBeforToDeliver(valueToCheck: Int): String {
-        var v = ""
-        for (k in _d.keys) {
-            if (valueToCheck == k) {
-                v = _d[k]!!
-                break
-            }
+    private fun mainProcess(args: Int): String {
+        return when (args) {
+            in 17..19 -> processSeventeenToNineteen(args)
+            in 21..69 -> processFromTwentyToSixty(args)
+            in 70..79 -> processSeventies(args)
+            in 80..89 -> processEighties(args)
+            in 90..99 -> processNineties(args)
+            in 101..999 -> processHundreds(args)
+            in 1001..999999 -> processThousands(args)
+            else -> getValueInMap(args)
         }
-        if (v.isBlank()) {
-            v = "Le chiffre $valueToCheck n'est pas encore pris en charge"
-        }
-        return v
     }
 
     private fun getValueInMap(args: Int): String {
-        return checkValueBeforToDeliver(args)
-    }
-
-    private fun processTeens(args: Int): String {
-        _lastDigitInString = args.toString().last().toString()
-        return  if(args in 17..19) {
-            _digit = getValueInMap(args - _lastDigitInString.toInt())
-            _digit2 = getValueInMap(_lastDigitInString.toInt())
-            "$_digit-${_digit2.toLowerCase()}"
+        return if (checkTheValueInTheMap(args)) {
+            _mapOfKnewDigit[args]!!
         } else {
-            getValueInMap(args)
+            "Le chiffre $args n'est pas encore pris en charge"
         }
     }
 
-    private fun processTwenties(args: Int): String {
-        _lastDigitInString = args.toString().last().toString()
-        return  if(args in 21..29) {
-            if (args == 21) {
-                _digit = getValueInMap(args - args.toString().last().toString().toInt())
-                _digit2 = getValueInMap(_lastDigitInString.toInt()).toLowerCase()
-                _digitInSting = "$_digit et $_digit2"
-            } else if (args in 22..29) {
-                _digit = getValueInMap(args - args.toString().last().toString().toInt())
-                _digit2 = getValueInMap(_lastDigitInString.toInt()).toLowerCase()
-                _digitInSting = "$_digit $_digit2"
-            }
-            _digitInSting
-        } else  {
-            getValueInMap(args)
-        }
+    private fun checkTheValueInTheMap(valueToCheck: Int): Boolean {
+        return _mapOfKnewDigit.containsKey(valueToCheck)
     }
 
-    private fun processThirties(args: Int): String {
-        _lastDigitInString = args.toString().last().toString()
-        return if (args in 31..39) {
-            if (args == 31) {
-                _digit = getValueInMap(args - args.toString().last().toString().toInt())
-                _digit2 = getValueInMap(_lastDigitInString.toInt()).toLowerCase()
-                _digitInSting = "$_digit et $_digit2"
-            } else if (args in 32..39) {
-                _digit = getValueInMap(args - args.toString().last().toString().toInt())
-                _digit2 = getValueInMap(_lastDigitInString.toInt()).toLowerCase()
-                _digitInSting = "$_digit $_digit2"
-            }
-            _digitInSting
-        } else {
-            getValueInMap(args)
-        }
+    private fun processSeventeenToNineteen(args: Int): String {
+        val lastDigit = args.toString().last().toString().toInt()
+        return  "${getValueInMap(args - lastDigit)}-${getValueInMap(lastDigit).toLowerCase()}"
     }
 
-    private  fun processForties(args: Int): String {
-        _lastDigitInString = args.toString().last().toString()
-        return if(args in 41..49) {
-            if (args == 41) {
-                _digit = getValueInMap(args - args.toString().last().toString().toInt())
-                _digit2 = getValueInMap(_lastDigitInString.toInt()).toLowerCase()
-                _digitInSting = "$_digit et $_digit2"
-            } else if (args in 42..49) {
-                _digit = getValueInMap(args - args.toString().last().toString().toInt())
-                _digit2 = getValueInMap(_lastDigitInString.toInt()).toLowerCase()
-                _digitInSting = "$_digit $_digit2"
-            }
-            _digitInSting
-        } else {
+    private fun processFromTwentyToSixty(args: Int): String {
+        val lastDigit = args.toString().last().toString().toInt()
+        return if (args == 30 || args == 40 || args == 50 || args == 60) {
             getValueInMap(args)
-        }
-    }
-
-    private fun processFifties(args: Int): String {
-        _lastDigitInString = args.toString().last().toString()
-        return if(args in 51..59) {
-            if (args == 51) {
-                _digit = getValueInMap(args - args.toString().last().toString().toInt())
-                _digit2 = getValueInMap(_lastDigitInString.toInt()).toLowerCase()
-                _digitInSting = "$_digit et $_digit2"
-            } else if (args in 52..59) {
-                _digit = getValueInMap(args - args.toString().last().toString().toInt())
-                _digit2 = getValueInMap(_lastDigitInString.toInt()).toLowerCase()
-                _digitInSting = "$_digit $_digit2"
-            }
-            _digitInSting
+        } else if (args == 21 || args == 31 || args == 41 || args == 51 || args == 61) {
+            "${getValueInMap(args - lastDigit)} et ${getValueInMap(lastDigit).toLowerCase()}"
         } else {
-            getValueInMap(args)
-        }
-    }
-
-    private fun processSixties(args: Int): String {
-        _lastDigitInString = args.toString().last().toString()
-        _digitInSting = ""
-        return if(args in 61..69) {
-            if (args == 61) {
-                _digit = getValueInMap(args - args.toString().last().toString().toInt())
-                _digit2 = getValueInMap(_lastDigitInString.toInt()).toLowerCase()
-                _digitInSting = "$_digit et $_digit2"
-            } else if (args in 62..69) {
-                _digit = getValueInMap(args - args.toString().last().toString().toInt())
-                _digit2 = getValueInMap(_lastDigitInString.toInt()).toLowerCase()
-                _digitInSting = "$_digit $_digit2"
-            }
-            _digitInSting
-        } else {
-            getValueInMap(args)
+            "${getValueInMap(args - lastDigit)} ${getValueInMap(lastDigit).toLowerCase()}"
         }
     }
 
     private  fun processSeventies(args: Int): String {
         return if(args in 71..79) {
-            if (args == 71) {
-                val digit = processSixties(60)
-                val digit2 = processTeens(args - 60).toLowerCase()
-                _digitInSting = "$digit et $digit2"
-            } else if (args in 72..79) {
-                val digit = processSixties(60)
-                val digit2 = processTeens(args - 60).toLowerCase()
-                _digitInSting = "$digit-$digit2"
-
+            val digitInLetter =  if (args == 71) {
+                "${getValueInMap(60)} et ${getValueInMap(args - 60).toLowerCase()}"
+            } else {
+                "${getValueInMap(60)}-${digitToLetter(args - 60).toLowerCase()}"
             }
-            _digitInSting
+            digitInLetter
         } else {
-            "${processSixties(60 )}-${processTeens(args - 60).toLowerCase()}"
+            "${getValueInMap(60)} ${getValueInMap(args - 60).toLowerCase()}"
         }
     }
 
     private fun processEighties(args: Int): String {
+        val lastDigit = args.toString().last().toString().toInt()
         return if (args in 81..89) {
-            _digit = getValueInMap(4)
-            _digit2 = getValueInMap(20).toLowerCase()
-            _digit3 = getValueInMap(args.toString().last().toString().toInt()).toLowerCase()
-            "$_digit-$_digit2-$_digit3"
+            "${getValueInMap(4)}-${getValueInMap(20).toLowerCase()}-${getValueInMap(lastDigit).toLowerCase()}"
         } else {
-            _digit = getValueInMap(4)
-            _digit2 = getValueInMap(20).toLowerCase()
-            _digit + "-" + _digit2 + "s"
+            "${getValueInMap(4)}-${getValueInMap(20).toLowerCase()}s"
         }
     }
 
     private  fun processNineties(args: Int): String {
-        val digit = getValueInMap(4)
-        val digit2 = getValueInMap(20).toLowerCase()
-        val digit3 = processTeens(args - 80).toLowerCase()
-        return "$digit-$digit2-$digit3"
+        return "${getValueInMap(4)}-${getValueInMap(20).toLowerCase()}-${digitToLetter(args - 80).toLowerCase()}"
     }
 
     private fun processHundreds(args: Int): String {
-        val digitToString: String
-        return if (args > 100) {
-            val firstDigit = args.toString().first().toString().toInt()
-            val middleDigit = args.toString()[1].toString().toInt()
-            digitToString = if (firstDigit == 1) {
-                commonProcessForHundreds(middleDigit, args, firstDigit.toString())
-            } else{
-                if (args == 200 || args == 300 || args == 400 || args == 500 || args == 600 || args == 700 || args == 800 || args == 900) {
-                    "${getValueInMap(firstDigit)} ${getValueInMap(100).toLowerCase()}s"
-                } else {
-                    "${getValueInMap(firstDigit)} ${commonProcessForHundreds(middleDigit, args, firstDigit.toString()).toLowerCase()}"
+        val firstDigit = args.toString().first().toString().toInt()
+        val middleDigit = args.toString()[1].toString().toInt()
+        fun commonProcessForHundreds(): String {
+            val d: Int
+            return if(middleDigit != 0) {
+                d = args.toString().removePrefix(firstDigit.toString()).toInt()
+                "${getValueInMap(100)} ${digitToLetter(d).toLowerCase()}"
+            } else {
+                d = args.toString().removeRange(0,1).toInt()
+                "${getValueInMap(100)} ${getValueInMap(d).toLowerCase()}"
+            }
+        }
+        return if (firstDigit == 1) {
+            commonProcessForHundreds()
+        } else {
+            if (args == 200 || args == 300 || args == 400 || args == 500 || args == 600 || args == 700 || args == 800 || args == 900) {
+                "${getValueInMap(firstDigit)} ${getValueInMap(100).toLowerCase()}s"
+            } else {
+                "${getValueInMap(firstDigit)} ${commonProcessForHundreds().toLowerCase()}"
+            }
+        }
+    }
+
+    private fun processThousands(args: Int): String {
+        var firstDigit: Int? = null
+        var secondDigit: Int? = null
+        var thirdDigit: Int? = null
+        var lastDigit: Int? = null
+        fun commonProcessForThousands(): String {
+            val d: Int
+            return if (secondDigit != 0) {
+                d = args.toString().removePrefix(firstDigit.toString()).toInt()
+                "${getValueInMap(1000)} ${digitToLetter(d).toLowerCase()}"
+            } else if (secondDigit == 0 && thirdDigit != 0) {
+                d = args.toString().removePrefix(firstDigit.toString()).toInt()
+                "${getValueInMap(1000)} ${digitToLetter(d).toLowerCase()}"
+            } else {
+                "${getValueInMap(1000)} ${getValueInMap(lastDigit!!).toLowerCase()}"
+            }
+        }
+        fun setFirstToLastDigitInThousands() {
+            val numberOfDigit = args.toString().count()
+            fun cPTSSecondToLastDigitInThousands(args: Int) {
+                val remainder: String = args.toString().removePrefix(firstDigit.toString())
+                secondDigit = remainder.first().toString().toInt()
+                thirdDigit = remainder[remainder.count() - 2].toString().toInt()
+                lastDigit = remainder.last().toString().toInt()
+            }
+            when (numberOfDigit) {
+                4 -> {
+                    firstDigit = args.toString().first().toString().toInt()
+                    cPTSSecondToLastDigitInThousands(args)
+                }
+                5 -> {
+                    firstDigit = args.toString().removeRange(2, 5).toInt()
+                    cPTSSecondToLastDigitInThousands(args)
+                }
+                6 -> {
+                    firstDigit = args.toString().removeRange(3, 6).toInt()
+                    cPTSSecondToLastDigitInThousands(args)
                 }
             }
-                digitToString
-        } else {
-            getValueInMap(args)
         }
-    }
-
-    private fun commonProcessForHundreds(middleDigit: Int, digitToBeTransform: Int, firstDigit: String): String {
-        return if(middleDigit != 0) {
-            val d = digitToBeTransform.toString().removePrefix(firstDigit).toInt()
-            "${getValueInMap(100)} ${digitToLetter(d).toLowerCase()}"
+        setFirstToLastDigitInThousands()
+        return if (args in 1000..1999) {
+            commonProcessForThousands()
         } else {
-            val d = digitToBeTransform.toString().removeRange(0,1).toInt()
-            "${getValueInMap(100)} ${getValueInMap(d).toLowerCase()}"
-        }
-    }
-
-    private fun mainProcess(args: Int) {
-        _numberInLetter = when (args) {
-            in 10..19 -> processTeens(args)
-            in 20..29 -> processTwenties(args)
-            in 30..39 -> processThirties(args)
-            in 40..49 -> processForties(args)
-            in 50..59 -> processFifties(args)
-            in 60..69 -> processSixties(args)
-            in 70..79 -> processSeventies(args)
-            in 80..89 -> processEighties(args)
-            in 90..99 -> processNineties(args)
-            in 100..999 -> processHundreds(args)
-            else -> getValueInMap(args)
+            if (secondDigit == 0 && secondDigit == 0 && lastDigit == 0){
+                "${digitToLetter(firstDigit!!)} ${getValueInMap(1000).toLowerCase()}"
+            } else {
+                "${digitToLetter(firstDigit!!)} ${commonProcessForThousands().toLowerCase()}"
+            }
         }
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,6 @@
 import judi.DigitToLetter
 
 fun main(args: Array<String>) {
-//    val d = DigitToLetter()
-//    println(d.digitToLetter(299))
     val d = DigitToLetter()
-    for (i in 0..1000) {
-        println(d.digitToLetter(i))
-    }
+    println(d.digitToLetter(999))
 }
